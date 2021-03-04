9aa1b07180083eddb19041ffeed357943f4c5c8d
MikkisGuy <69403498+MikkisGuy@users.noreply.github.com>
2021-03-04 22:15:48 +0200

    Fixed "Missing return type on function" warning

--- hangman-frontend/src/App.tsx
+++ hangman-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function App() {
-  return <>Hello World</>;
+function App(): JSX.Element {
+  return (<>Hello World</>);
 }
 
-export default App;
+export default App; 
